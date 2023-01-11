5a44f56eaa31ecc9d1b397ed6a293d9cb7ca1845
Justin Wozniak <woz@anl.gov>
2023-01-10 19:00:59 -0600

    Working version

---
+++ pr-1.swift
@@ -0,0 +1,50 @@
+
+import files;
+import io;
+import sys;
+
+SLICES     = 5;
+ITERATIONS = 5;
+
+// D[iteration][slice]
+int D[][];
+
+app (file f) simulation(int start, int i, int s, int v)
+{
+  "./simulation.sh" start i s v f ;
+}
+
+start = clock_seconds();
+
+foreach iteration in [1:ITERATIONS]
+{
+  foreach slice in [1:SLICES]
+  {
+    printf("iteration: %i slice: %i", iteration, slice);
+    name = "out-%i-%i.txt" % (iteration, slice);
+    if (iteration == 1 && slice == 1) // base case (lower left)
+    {
+      file f<name>;
+      f = simulation(start, iteration, slice, 0);
+      int value = string2int(read(f));
+      D[1][1] = value;
+    }
+    else if (iteration <= slice)
+    {
+      if (iteration == 1) // left column
+      {
+        file f<name>;
+        f = simulation(start, iteration, slice, D[iteration][slice-1]);
+        int value = string2int(read(f));
+        D[iteration][slice] = value;
+      }
+      else // remaining cells
+      {
+        file f<name>;
+        f = simulation(start, iteration, slice, D[iteration-1][slice]);
+        int value = string2int(read(f));
+        D[iteration][slice] = value;
+      }
+    }
+  }
+}
---
+++ simulation.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -eu
+
+# SIMULATION SH
+
+if (( ${#} != 5 ))
+then
+  echo "simulation.sh: Provide START ITERATION SLICE INPUT OUTPUT"
+  exit 1
+fi
+
+START=$1
+ITERATION=$2
+SLICE=$3
+INPUT=$4
+OUTPUT=$5
+
+TIME=$(( $( date +%s ) - START ))
+VALUE=$RANDOM
+printf "simulation.sh: TIME %3i   ITERATION %2i   SLICE %2i   INPUT %5i   VALUE %5i\n" \
+       $TIME $ITERATION $SLICE $INPUT $VALUE
+
+sleep 3
+
+# Write an output value:
+echo $VALUE > $OUTPUT
